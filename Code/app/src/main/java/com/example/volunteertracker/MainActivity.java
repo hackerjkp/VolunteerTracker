ac58013e56f87480b37ffac715bda2b359af1535
odnaiviv <viviando.0211@gmail.com>
2023-12-02 14:00:58 -0500

    josh's code

--- Code/app/src/main/java/com/example/volunteertracker/MainActivity.java
+++ Code/app/src/main/java/com/example/volunteertracker/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.volunteertracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Variables for check in and check out messages
+        Button checkIn = (Button)findViewById(R.id.checkIn);
+        TextView checkInMessage = (TextView)findViewById(R.id.checkInMessage);
+        Button checkOut = (Button)findViewById(R.id.checkOut);
+        TextView checkOutMessage = (TextView)findViewById(R.id.checkOutMessage);
+
+        //display check in message
+        checkIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                checkInMessage.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //display check out message
+        checkOut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                checkOutMessage.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 }
